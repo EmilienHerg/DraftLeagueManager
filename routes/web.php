cf39d21dd4f4a0f42d77f20439ac8ac67f7f5691
EmilienHerg <emilien.herg@gmail.com>
2025-11-30 13:53:09 +0100

    install react

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inscription', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/inscription', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'showLogionForm'])->name('login');
+Route::post('/login', [AuthController::class], 'login');
